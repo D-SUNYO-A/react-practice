ffeeaec5e42a783afa3eb4f1c0988b74b9b21ce4
D-SUNYO-A <sdanarson1@gmail.com>
2024-03-28 18:07:00 +0300

    format-prettier

--- src/containers/auth/Register.jsx
+++ src/containers/auth/Register.jsx
@@ -1,54 +1,60 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 const Register = () => {
   return (
-    <section className="text-gray-600 body-font flex min-h-screen items-center">
-      <div className="container px-5 py-24 mx-auto flex flex-wrap items-center">
-        <div className="lg:w-3/5 md:w-1/2 md:pr-16 lg:pr-0 pr-0">
-          <h1 className="title-font font-medium text-3xl text-gray-900">
+    <section className="flex items-center min-h-screen text-gray-600 body-font">
+      <div className="container flex flex-wrap items-center px-5 py-24 mx-auto">
+        <div className="pr-0 lg:w-3/5 md:w-1/2 md:pr-16 lg:pr-0">
+          <h1 className="text-3xl font-medium text-gray-900 title-font">
             Register here to join us
           </h1>
-          <p className="leading-relaxed mt-4">
-            Tongava maro oh !
-          </p>
+          <p className="mt-4 leading-relaxed">Tongava maro oh !</p>
         </div>
-        <div className="lg:w-2/6 md:w-1/2 bg-gray-100 rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0">
-          <h2 className="text-gray-900 text-lg font-medium title-font mb-5">
+        <div className="flex flex-col w-full p-8 mt-10 bg-gray-100 rounded-lg lg:w-2/6 md:w-1/2 md:ml-auto md:mt-0">
+          <h2 className="mb-5 text-lg font-medium text-gray-900 title-font">
             Sign Up
           </h2>
           <div className="relative mb-4">
-            <label htmlFor="full-name" className="leading-7 text-sm text-gray-600">
+            <label
+              htmlFor="full-name"
+              className="text-sm leading-7 text-gray-600"
+            >
               Full Name
             </label>
             <input
               type="text"
               id="full-name"
               name="full-name"
-              className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+              className="w-full px-3 py-1 text-base leading-8 text-gray-700 transition-colors duration-200 ease-in-out bg-white border border-gray-300 rounded outline-none focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200"
             />
           </div>
           <div className="relative mb-4">
-            <label htmlFor="email" className="leading-7 text-sm text-gray-600">
+            <label htmlFor="email" className="text-sm leading-7 text-gray-600">
               Email
             </label>
             <input
               type="email"
               id="email"
               name="email"
-              className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+              className="w-full px-3 py-1 text-base leading-8 text-gray-700 transition-colors duration-200 ease-in-out bg-white border border-gray-300 rounded outline-none focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200"
             />
           </div>
-          <Link to={`/app`} className="text-white bg-indigo-500 border-0 my-3 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg text-center">
+          <Link
+            to={`/app`}
+            className="px-8 py-2 my-3 text-lg text-center text-white bg-indigo-500 border-0 rounded focus:outline-none hover:bg-indigo-600"
+          >
             Save
           </Link>
           <p className="text-xs text-gray-500">
-            If you already have an accoumpte, you can directly <Link to={`/login`} className="text-blue-400 hover:underline">log here</Link>
+            If you already have an accoumpte, you can directly{" "}
+            <Link to={`/login`} className="text-blue-400 hover:underline">
+              log here
+            </Link>
           </p>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
